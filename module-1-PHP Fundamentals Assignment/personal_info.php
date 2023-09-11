45b1fb40c81e126f40b97f999a1b517aeb399cb7
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-09-12 02:24:28 +0600

    Some modification

--- module-1-PHP Fundamentals Assignment/personal_info.php
+++ module-1-PHP Fundamentals Assignment/personal_info.php
@@ -1,10 +1,16 @@
 <?php
 $my_name = 'Firoz Rahman Sabuz';
 $age = 35;
-$country = 'Bangladesh';
-$brief_introduction = " I am a php developer, I want to learn more. ";
+$country = 'Bangladesh .';
+$brief_introduction = " I am a php, Laravel developer, I want to learn more. ";
 
-echo $my_name .'</br>';
-echo $age .'</br>';
-echo $country .'</br>';
-echo $brief_introduction .'</br>';
+printf("<h1>My name is %s and age is %d and my country is %s </h1>",$my_name,$age,$country );
+
+printf("<h2>About brief information about mine is- %s</h2>",$brief_introduction);
+
+echo '<b>';
+echo 'My name is: '. $my_name .' .</br>';
+echo 'I am '. $age .' years old .</br>';
+echo 'I live in '.$country .' </br>';
+echo 'About Myself: '. $brief_introduction .'</br>';
+echo '</b>';
